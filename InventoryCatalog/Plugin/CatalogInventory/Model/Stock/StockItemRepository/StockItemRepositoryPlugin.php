f26a7a62d22c29ee55ebee2456956caa2a2a247d
lakshmana49 <glo28218@adobe.com>
2024-11-12 12:09:49 +0530

    ACP2E-3355: Cron reindex fails to update product availability on the frontend

--- InventoryCatalog/Plugin/CatalogInventory/Model/Stock/StockItemRepository/StockItemRepositoryPlugin.php
+++ InventoryCatalog/Plugin/CatalogInventory/Model/Stock/StockItemRepository/StockItemRepositoryPlugin.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright 2024 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
 
 namespace Magento\InventoryCatalog\Plugin\CatalogInventory\Model\Stock\StockItemRepository;
 
