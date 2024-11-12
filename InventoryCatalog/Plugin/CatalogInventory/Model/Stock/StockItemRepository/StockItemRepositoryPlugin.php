d40914e45d14ca50fdea9f4440cb58ceb7e1ac6f
lakshmana49 <glo28218@adobe.com>
2024-11-12 08:53:42 +0530

    ACP2E-3355: Cron reindex fails to update product availability on the frontend

---
+++ InventoryCatalog/Plugin/CatalogInventory/Model/Stock/StockItemRepository/StockItemRepositoryPlugin.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Magento\InventoryCatalog\Plugin\CatalogInventory\Model\Stock\StockItemRepository;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Indexer\Product\Full as FullProductIndexer;
+use Magento\CatalogInventory\Api\Data\StockItemInterface;
+use Magento\CatalogInventory\Model\Stock\StockItemRepository;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Inventory\Model\SourceItem\Command\GetSourceItemsBySku;
+use Magento\InventoryIndexer\Indexer\InventoryIndexer;
+
+class StockItemRepositoryPlugin
+{
+    /**
+     * @var FullProductIndexer
+     */
+    private $fullProductIndexer;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $inventoryIndexer;
+
+    /**
+     * @var GetSourceItemsBySku
+     */
+    private $getSourceItemsBySku;
+
+    /**
+     * @param FullProductIndexer $fullProductIndexer
+     * @param InventoryIndexer $inventoryIndexer
+     * @param ProductRepositoryInterface $productRepository
+     * @param GetSourceItemsBySku $getSourceItemsBySku
+     */
+    public function __construct(
+        FullProductIndexer $fullProductIndexer,
+        InventoryIndexer $inventoryIndexer,
+        ProductRepositoryInterface $productRepository,
+        getSourceItemsBySku $getSourceItemsBySku
+    ) {
+        $this->fullProductIndexer = $fullProductIndexer;
+        $this->inventoryIndexer = $inventoryIndexer;
+        $this->productRepository = $productRepository;
+        $this->getSourceItemsBySku = $getSourceItemsBySku;
+    }
+
+    /**
+     * Complex reindex after product stock item has been saved.
+     *
+     * @param StockItemRepository $subject
+     * @param StockItemInterface $stockItem
+     * @return StockItemInterface
+     * @throws NoSuchEntityException
+     */
+    public function afterSave(StockItemRepository $subject, StockItemInterface $stockItem): StockItemInterface
+    {
+        $product = $this->productRepository->getById($stockItem->getProductId());
+        $this->fullProductIndexer->executeRow($product->getId());
+        $sourceItems = $this->getSourceItemsBySku->execute($product->getSku());
+        $sourceItemIds = [];
+
+        foreach ($sourceItems as $sourceItem) {
+            $sourceItemIds[] = $sourceItem->getId();
+        }
+        $this->inventoryIndexer->executeList($sourceItemIds);
+        return $stockItem;
+    }
+}
