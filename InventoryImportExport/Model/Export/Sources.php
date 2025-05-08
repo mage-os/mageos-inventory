c12ba2618897d71ae11bc8234bc2f8fc3d2754f8
flowers <flowers@adobe.com>
2025-05-08 14:21:37 -0500

    ACP2E-3863: Update copyrights for MSI and fix static errors. Modules: InventoryReservationsApi, InventoryImportExport

--- InventoryImportExport/Model/Export/Sources.php
+++ InventoryImportExport/Model/Export/Sources.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2017 Adobe
+ * All Rights Reserved.
  */
 declare(strict_types=1);
 
@@ -75,6 +75,7 @@ class Sources extends AbstractEntity
 
     /**
      * @inheritdoc
+     *
      * @throws Exception
      */
     public function export()
@@ -98,6 +99,7 @@ class Sources extends AbstractEntity
 
     /**
      * @inheritdoc
+     *
      * @throws Exception
      */
     protected function _getHeaderColumns()
