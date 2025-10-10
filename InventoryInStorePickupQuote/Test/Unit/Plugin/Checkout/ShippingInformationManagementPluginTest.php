22c769218800de6caa391979d503c967a49b6bc4
Alexandra Zota <zota@adobe.com>
2025-10-10 21:12:54 +0300

    ACP2E-4260: [MSI] Failing MFTF tests related to latest mainline changes.

--- InventoryInStorePickupQuote/Test/Unit/Plugin/Checkout/ShippingInformationManagementPluginTest.php
+++ InventoryInStorePickupQuote/Test/Unit/Plugin/Checkout/ShippingInformationManagementPluginTest.php
@@ -90,7 +90,7 @@ class ShippingInformationManagementPluginTest extends TestCase
     }
 
     /**
-     * Test beforeSaveAddressInformation when billing address is incomplete
+     * Test beforeSaveAddressInformation when it's pickup store with incomplete billing
      *
      * @return void
      */
@@ -104,49 +104,6 @@ class ShippingInformationManagementPluginTest extends TestCase
         $this->extensionAttributes->expects($this->once())
             ->method('getPickupLocationCode')
             ->willReturn($pickupLocationCode);
-        $this->billingAddress->expects($this->once())
-            ->method('getFirstname')
-            ->willReturn(null);
-        $this->addressInformation->expects($this->once())
-            ->method('getShippingAddress')
-            ->willReturn($this->shippingAddress);
-        $this->addressInformation->expects($this->once())
-            ->method('getBillingAddress')
-            ->willReturn($this->billingAddress);
-        $this->addressInformation->expects($this->once())
-            ->method('setBillingAddress')
-            ->with($this->shippingAddress);
-        $result = $this->plugin->beforeSaveAddressInformation(
-            $this->subject,
-            $cartId,
-            $this->addressInformation
-        );
-        $this->assertEquals([$cartId, $this->addressInformation], $result);
-    }
-
-    /**
-     * Test beforeSaveAddressInformation when billing address is complete
-     *
-     * @return void
-     */
-    public function testBeforeSaveAddressInformationPickupStoreCompleteBilling(): void
-    {
-        $cartId = 123;
-        $pickupLocationCode = 'store_001';
-        $this->shippingAddress->expects($this->once())
-            ->method('getExtensionAttributes')
-            ->willReturn($this->extensionAttributes);
-        $this->extensionAttributes->expects($this->once())
-            ->method('getPickupLocationCode')
-            ->willReturn($pickupLocationCode);
-        $this->billingAddress->expects($this->once())->method('getFirstname')->willReturn('Test');
-        $this->billingAddress->expects($this->once())->method('getLastname')->willReturn('Test');
-        $this->billingAddress->expects($this->once())->method('getStreet')->willReturn(['123 Test Street']);
-        $this->billingAddress->expects($this->once())->method('getCity')->willReturn('Austin');
-        $this->billingAddress->expects($this->once())->method('getPostcode')->willReturn('78758');
-        $this->billingAddress->expects($this->once())->method('getTelephone')->willReturn('512-1234567');
-        $this->billingAddress->expects($this->once())->method('getRegionId')->willReturn(12);
-        $this->billingAddress->expects($this->once())->method('getCountryId')->willReturn('US');
         $this->addressInformation->expects($this->once())
             ->method('getShippingAddress')
             ->willReturn($this->shippingAddress);
@@ -163,7 +120,37 @@ class ShippingInformationManagementPluginTest extends TestCase
     }
 
     /**
-     * Test beforeSaveAddressInformation when pickup store shipping is false
+     * Test beforeSaveAddressInformation when it's pickup store with complete billing
+     *
+     * @return void
+     */
+    public function testBeforeSaveAddressInformationPickupStoreCompleteBilling(): void
+    {
+        $cartId = 123;
+        $pickupLocationCode = 'store_001';
+        $this->shippingAddress->expects($this->once())
+            ->method('getExtensionAttributes')
+            ->willReturn($this->extensionAttributes);
+        $this->extensionAttributes->expects($this->once())
+            ->method('getPickupLocationCode')
+            ->willReturn($pickupLocationCode);
+        $this->addressInformation->expects($this->once())
+            ->method('getShippingAddress')
+            ->willReturn($this->shippingAddress);
+        $this->addressInformation->expects($this->once())
+            ->method('getBillingAddress')
+            ->willReturn($this->billingAddress);
+        $this->addressInformation->expects($this->never())->method('setBillingAddress');
+        $result = $this->plugin->beforeSaveAddressInformation(
+            $this->subject,
+            $cartId,
+            $this->addressInformation
+        );
+        $this->assertEquals([$cartId, $this->addressInformation], $result);
+    }
+
+    /**
+     * Test beforeSaveAddressInformation when it's not pickup store and billing is null
      *
      * @return void
      */
@@ -181,8 +168,10 @@ class ShippingInformationManagementPluginTest extends TestCase
             ->willReturn($this->shippingAddress);
         $this->addressInformation->expects($this->once())
             ->method('getBillingAddress')
-            ->willReturn($this->billingAddress);
-        $this->addressInformation->expects($this->never())->method('setBillingAddress');
+            ->willReturn(null);
+        $this->addressInformation->expects($this->once())
+            ->method('setBillingAddress')
+            ->with($this->shippingAddress);
         $result = $this->plugin->beforeSaveAddressInformation(
             $this->subject,
             $cartId,
